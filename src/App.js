31204aba1a605381d30b425409e308a501fae356
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-13 11:23:48 +0100

    complete the dynamic part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    step < 3 && setStep(step + 1);
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <div className="message"></div>
+      <div className="message">
+        Step {step}: {messages[step - 1]}
+      </div>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   );
